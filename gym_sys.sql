8f4c507828bb98b167f91b34a1fc13ff23c0fb39
Ahmad Alqunbar <88638082+Ahmad-Alqunbar@users.noreply.github.com>
2024-02-20 22:26:24 +0300

    Add files via upload

---
+++ gym_sys.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 20, 2024 at 08:24 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gym_sys`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+-- Error reading structure for table gym_sys.members: #1932 - Table &#039;gym_sys.members&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.members: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`members`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+-- Error reading structure for table gym_sys.packages: #1932 - Table &#039;gym_sys.packages&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.packages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`packages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+-- Error reading structure for table gym_sys.permissions: #1932 - Table &#039;gym_sys.permissions&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`permissions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+-- Error reading structure for table gym_sys.roles: #1932 - Table &#039;gym_sys.roles&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`roles`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_permissions`
+--
+-- Error reading structure for table gym_sys.role_permissions: #1932 - Table &#039;gym_sys.role_permissions&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.role_permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`role_permissions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trainers`
+--
+-- Error reading structure for table gym_sys.trainers: #1932 - Table &#039;gym_sys.trainers&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.trainers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`trainers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table gym_sys.users: #1932 - Table &#039;gym_sys.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_color_settings`
+--
+-- Error reading structure for table gym_sys.user_color_settings: #1932 - Table &#039;gym_sys.user_color_settings&#039; doesn&#039;t exist in engine
+-- Error reading data for table gym_sys.user_color_settings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gym_sys`.`user_color_settings`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
